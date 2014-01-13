8e0ae70217f05bb852f2abf37654149733a15c4e
James Vastbinder <james.vastbinder@gmail.com>
2014-01-13 15:11:59 -0800

    upgrading ember-app-kit

--- tasks/options/watch.js
+++ tasks/options/watch.js
@@ -1,30 +1,44 @@
-var Helpers = require('../helpers');
+var Helpers = require('../helpers'),
+    filterAvailable = Helpers.filterAvailableTasks;
 
-var scripts = '{app,tests}/**/*.{js,coffee}';
-var templates = 'app/templates/**/*.{hbs,handlebars,hjs,emblem}';
-var styles = 'app/styles/**/*.{css,sass,scss,less,styl}';
-var other = '{app,tests,public,vendor}/**/*';
+var scripts = '{app,tests}/**/*.{js,coffee,em}',
+    templates = 'app/templates/**/*.{hbs,handlebars,hjs,emblem}',
+    sprites = 'app/sprites/**/*.{png,jpg,jpeg}',
+    styles = 'app/styles/**/*.{css,sass,scss,less,styl}',
+    indexHTML = 'app/index.html',
+    other = '{app,tests,public,vendor}/**/*';
 
 module.exports = {
   scripts: {
     files: [scripts],
-    tasks: ['lock', 'buildScripts', 'unlock', 'karma:server:run']
+    tasks: ['lock', 'buildScripts', 'unlock']
   },
   templates: {
     files: [templates],
-    tasks: ['lock', 'buildTemplates:debug', 'unlock', 'karma:server:run']
+    tasks: ['lock', 'buildTemplates:debug', 'unlock']
+  },
+  sprites: {
+    files: [sprites],
+    tasks: filterAvailable(['lock', 'fancySprites:create', 'unlock'])
   },
   styles: {
     files: [styles],
-    tasks: ['lock', 'buildStyles', 'unlock', 'karma:server:run']
+    tasks: ['lock', 'buildStyles', 'unlock']
+  },
+  indexHTML: {
+    files: [indexHTML],
+    tasks: ['lock', 'buildIndexHTML:debug', 'unlock']
   },
   other: {
-    files: [other, '!'+scripts, '!'+templates, '!'+styles],
-    tasks: ['build:debug', 'karma:server:run']
+    files: [other, '!'+scripts, '!'+templates, '!'+styles, '!'+indexHTML],
+    tasks: ['lock', 'build:debug', 'unlock']
   },
 
   options: {
-    debounceDelay: 200,
+    // No need to debounce
+    debounceDelay: 0,
+    // When we don't have inotify
+    interval: 100,
     livereload: Helpers.isPackageAvailable("connect-livereload")
   }
 };
